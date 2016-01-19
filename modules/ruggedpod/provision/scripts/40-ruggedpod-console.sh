a9af51ac7b2d7cea54b52557a0e3fea04d16fc36
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-01-19 10:50:58 -0800

    Install the serial console

---
+++ modules/ruggedpod/provision/scripts/40-ruggedpod-console.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+apt-get install -y nodejs
+
+cd /tmp
+wget https://github.com/RuggedPOD/ruggedpod-web/releases/download/0.1.0/ruggedpod-serial.tar.gz
+mkdir -p /opt/ruggedpod-serial
+cd /opt/ruggedpod-serial
+tar xzf /tmp/ruggedpod-serial.tar.gz
+rm -rf /tmp/ruggedpod-serial.tar.gz
+chown -R ruggedpod: /opt/ruggedpod-serial
