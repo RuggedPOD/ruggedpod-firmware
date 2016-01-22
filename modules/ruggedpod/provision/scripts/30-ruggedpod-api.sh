668ceb5bf8b3a625c6d71925d864f0ed1989b5c1
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-01-22 15:01:33 -0800

    Fix RuggedPOD API installation
    Running a background command in the chroot context
    fails with the current setup. It was actually not
    necessary. This was only to be sure to write
    periodically in the log file to avoid travis think
    the build is stuck.
    
    Replacing it by a dummy script that writes in the
    log in a loop makes the trick even if it's pretty
    dirty.

--- modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
+++ modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-set -x
+set -eux
 
-pip install git+https://github.com/RuggedPOD/ruggedpod-api.git &
-
-pid=$!
-
-while true ; do
-    ps -o pid= -p $pid
-    if [ $? -ne 0 ] ; then
-        echo "RuggedPOD API installation is done"
-        exit 0
-    fi
-    sleep 60
-    echo "Waiting for RuggedPOD API installation to complete..."
-done
+pip install git+https://github.com/RuggedPOD/ruggedpod-api.git
