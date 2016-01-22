05d5d7d52995a117e75554be43ba621d09d3079a
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-01-21 16:21:08 -0800

    Avoid travis to stop the builing process
    The pip install involves a very long compile process.
    During that time, nothing is output on stdout.
    When no output occured during more than 10 minutes
    travis stops the build because it considers it to be
    stuck.

--- modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
+++ modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-set -eux
+set -x
 
-pip install git+https://github.com/RuggedPOD/ruggedpod-api.git
+pip install git+https://github.com/RuggedPOD/ruggedpod-api.git &
+
+pid=$!
+
+while true ; do
+    ps -o pid= -p $pid
+    if [ $? -ne 0 ] ; then
+        echo "RuggedPOD API installation is done"
+        exit 0
+    fi
+    sleep 60
+    echo "Waiting for RuggedPOD API installation to complete..."
+done
