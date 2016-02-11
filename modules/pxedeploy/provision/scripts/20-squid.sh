b4562971ea5227cc067c816ef5d33f7454a3ea8b
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-02-11 11:37:42 -0800

    Squid cache configuration and provisioning
    This allows to install a blade operating system without
    the need to be connected to internet.
    
    Currently the pre-loaded cache contains files to be able
    to install an Ubuntu Trusty.

--- modules/pxedeploy/provision/scripts/20-squid.sh
+++ modules/pxedeploy/provision/scripts/20-squid.sh
@@ -6,3 +6,13 @@ apt-get install -y squid
 service squid stop
 
 echo "192.168.2.15	ruggedpod" >> /etc/hosts
+
+dir=/var/cache/squid
+file=ruggedpod-firmware-cache-0.1.0.tar.gz
+
+mkdir -p ${dir}
+cd ${dir}
+wget https://s3-us-west-2.amazonaws.com/ruggedpod/public/${file}
+tar xvzf ${file}
+rm -f ${file}
+chown -R proxy: ${dir}
