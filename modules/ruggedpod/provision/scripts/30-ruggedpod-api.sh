6a25eb9aab162bb3e733d6f38c38957f60f0e92e
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-02-11 12:19:02 -0800

    Workaround a weird bug...
    A basic pip install raise a PBR error
    
    alueError: git history requires a target version of
    pbr.version.SemanticVersion(0.2.1), but target version is
    pbr.version.SemanticVersion(0.1.0)
    
    Have to investigate that point.

--- modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
+++ modules/ruggedpod/provision/scripts/30-ruggedpod-api.sh
@@ -2,7 +2,13 @@
 
 set -eux
 
-pip install git+https://github.com/RuggedPOD/ruggedpod-api.git
+cd /tmp
+git clone https://github.com/RuggedPOD/ruggedpod-api.git
+cd ruggedpod-api
+git checkout 2305c40
+pip install .
+cd ..
+rm -rf /tmp/ruggedpod-api
 
 mkdir -p /var/lib/ruggedpod
 chown ruggedpod: /var/lib/ruggedpod
