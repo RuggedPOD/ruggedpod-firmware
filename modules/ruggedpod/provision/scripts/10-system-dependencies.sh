33eee9301fab5955b5b7ae19302e30f343e6f795
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2016-08-10 21:29:03 +0200

    Add --force-yes option to avaid problems

--- modules/ruggedpod/provision/scripts/10-system-dependencies.sh
+++ modules/ruggedpod/provision/scripts/10-system-dependencies.sh
@@ -2,5 +2,6 @@
 
 set -eux
 
-apt-get install -y git apache2 apache2-dev python-dev python-pip libxml2-dev \
-                   libxslt1-dev zlib1g-dev libffi-dev libssl-dev python-smbus
+apt-get install -y --force-yes \
+             git apache2 apache2-dev python-dev python-pip libxml2-dev \
+             libxslt1-dev zlib1g-dev libffi-dev libssl-dev python-smbus
